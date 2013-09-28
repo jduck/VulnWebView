2173a97a3094cb670d034726e75bd3b23ca6462b
Joshua J. Drake <github.jdrake@qoop.org>
2013-09-27 22:53:32 -0500

    Initial commit

---
+++ src/org/droidsec/vulnwebview/MainActivity.java
@@ -0,0 +1,49 @@
+package org.droidsec.vulnwebview;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+        		WebView myWebView = (WebView) findViewById(R.id.webView1);
+        		myWebView.reload();
+            }
+        });
+		
+		WebView myWebView = (WebView) findViewById(R.id.webView1);
+		
+		// not a good idea!
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		
+		// terrible idea!
+		myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
+		
+		// woot.
+		myWebView.loadUrl("http://droidsec.org/addjsif.html");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ src/org/droidsec/vulnwebview/WebAppInterface.java
@@ -0,0 +1,19 @@
+package org.droidsec.vulnwebview;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class WebAppInterface {
+    Context mContext;
+    private String mWTF = "lolz";
+
+    /** Instantiate the interface and set the context */
+    WebAppInterface(Context c) {
+        mContext = c;
+    }
+
+    /** Show a toast from the web page */
+    public void showToast(String toast) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+    }
+}
